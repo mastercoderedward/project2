1ceb2d6bc50f465d56a064b305944699a1c4e485
mastercoderedward <48935742+mastercoderedward@users.noreply.github.com>
2019-03-28 09:35:24 -0700

    Update MovieRentApp.java

--- MovieRentApp.java
+++ MovieRentApp.java
@@ -5,127 +5,125 @@ import java.util.Scanner;
 
 public class MovieRentApp {
 
-    public static void main(String[] args) {
-        Menu();
-    }
+	public static void main(String[] args) throws FileNotFoundException {
+		Menu();
+	}
 
-    private static void Menu() {
+	private static void Menu() throws FileNotFoundException {
 
-        Scanner input = new Scanner(System.in);
-        int choice;
+		Scanner input = new Scanner(System.in);
+		int choice;
 
-        do {
-            System.out.println("1. Developers");
-            System.out.println("2. Search");
-            System.out.println("3. Rent");
-            System.out.println("4. Return");
-            System.out.println("5. Earnings");
-            System.out.println("6. Exit");
-            choice = input.nextInt();
+		do {
+			System.out.println("1. Developers");
+			System.out.println("2. Search");
+			System.out.println("3. Rent");
+			System.out.println("4. Return");
+			System.out.println("5. Earnings");
+			System.out.println("6. Exit");
+			choice = input.nextInt();
 
-            switch (choice) {
-            case 1:
-                System.out.println("Created by: Jonathan Chao & Edward Balase");
-                break;
-            case 2:
-                Search();
-                break;
-            case 3:
-                Rent();
-                break;
-            case 4:
-                Return();
-                break;
-            case 5:
-                Earnings();
-                break;
-            }
+			switch (choice) {
+			case 1:
+				System.out.println("Created by: Jonathan Chao & Edward Balase");
+				break;
+			case 2:
+				Search();
+				break;
+			case 3:
+				Rent();
+				break;
+			case 4:
+				Return();
+				break;
+			case 5:
+				Earnings();
+				break;
+			}
 
-        } while (choice != 6);
-        System.exit(0);
+		} while (choice != 6);
+		System.exit(0);
 
-    }
+	}
 
-    private static void Rent() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Which movies would you like to rent? \ntype e to exit to menu");
-        String userInput = input.nextLine();
-        while (userInput != "e") {
+	private static void Rent() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Which movies would you like to rent? \ntype e to exit to menu");
+		String userInput = input.nextLine();
+		while (userInput != "e") {
 
-        }
+		}
 
-        // Store in file of trancations and earnings file in earnings method
-        Earnings();
-    }
+		// Store in file of trancations and earnings file in earnings method
+		Earnings();
+	}
 
-    private static void Return() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Which movie would you like to return? \ntype e to exit to menu");
-        String userInput = input.nextLine();
-        while (userInput != "e") {
+	private static void Return() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Which movie would you like to return? \ntype e to exit to menu");
+		String userInput = input.nextLine();
+		while (userInput != "e") {
 
-        }
-    }
+		}
+	}
 
-    private static void Earnings() {
+	private static void Earnings() {
 
-    }
+	}
 
-    private static void Search() {
-        System.out.println("   a. All available movies");
-        System.out.println("   b. By genre (e.g Adventure, Horror etc...)");
-        System.out.println("   c. By Title");
-        System.out.println("   d. Go back to menu");
-        Scanner input = new Scanner(System.in);
+	private static void Search() throws FileNotFoundException {
+		System.out.println("   a. All available movies");
+		System.out.println("   b. By genre (e.g Adventure, Horror etc...)");
+		System.out.println("   c. By Title");
+		System.out.println("   d. Go back to menu");
+		Scanner input = new Scanner(System.in);
+		String userInput;
+		do {
+			userInput = input.next();
+			userInput.toLowerCase();
 
-        do {
-            String userInput = input.next();
-            userInput.toLowerCase();
+			switch (userInput) {
+			case "a":
+				System.out.println("All available movies: ");
+				availableMovie();
+				break;
+			case "b":
+				System.out.println("What type of genre are you looking for? ");
+				MovieGenre();
+				break;
+			case "c":
+				System.out.println("What is the title of the movie? ");
+				MovieTitle();
+				break;
+			}
+		} while (userInput != "d");
+	}
 
-            switch (userInput) {
-            case "a":
-                System.out.println("All available movies: ");
-                availableMovie();
-                break;
-            case "b":
-                System.out.println("What type of genre are you looking for? ");
-                MovieGenre();
-                break;
-            case "c":
-                System.out.println("What is the title of the movie? ");
-                MovieTitle();
-                break;
-            }
-        } while (userInput != "d");
-    }
+	private static void availableMovie() throws FileNotFoundException{
+		
+		String fileName = "Movie_Data.csv";
+		File file = new File(fileName);
+		Scanner input = new Scanner(file);
+		String movie = input.nextLine();
 
-    private static void availableMovie() {
-        Scanner input = new Scanner(System.in);
-        String movie = input.next();
+		while (input.hasNext()) {
+			
+			String data = input.nextLine();
+			String[] name = data.split("\"");
+			if(name.length != 3)
+				name = data.split(",");
+				System.out.println("	" + name[1]);
 
-        String fileName = "Movie_Data.csv";
-        File file = new File(fileName);
+		}
 
-        try {
-            Scanner input1 = new Scanner(file);
-            while (input1.hasNext()) {
-                String data = input1.nextLine();
-                String[] name = data.split(",");
-                if (name[1] == movie)
-                    System.out.println("	" + movie + " is available " + name[2]);
+	}
 
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+	private static void MovieGenre() {
 
-    private static void MovieGenre() {
+	}
 
-    }
+	private static void MovieTitle() {
 
-    private static void MovieTitle() {
-
-    }
+	}
 
 }
